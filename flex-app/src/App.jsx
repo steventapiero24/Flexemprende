03019bdb5a7d11d75d52235d5d73b5f8df4901b6
steventapiero24 <steventapiero24@gmail.com>
2025-11-04 11:46:32 +0100

    confg home

--- flex-app/src/App.jsx
+++ flex-app/src/App.jsx
@@ -1,11 +1,12 @@
 import './App.css'
+import Home from './pages/Home.jsx'
 
 function App() {
 
   return (
     <>
       <div>
-        <h1>Hola mundo</h1>
+        <Home />
       </div>
     </>
   )
