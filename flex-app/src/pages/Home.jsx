ae7ee3aac78ee3ef0a0c9ec04372cb1d99954712
steventapiero24 <steventapiero24@gmail.com>
2025-11-04 12:55:49 +0100

    Hero creation

--- flex-app/src/pages/Home.jsx
+++ flex-app/src/pages/Home.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import Nav from '../components/Nav'
+import Hero from '../components/Hero'
 
 const Home = () => {
   return (
     <>
      <Nav/>
+     <Hero/>  
     </>
   )
 }
