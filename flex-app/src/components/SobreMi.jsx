28d9dcd1e6171c681c3b73646a304866a2a4ec80
steventapiero24 <steventapiero24@gmail.com>
2025-11-10 18:59:23 +0100

    Sobre mi  falling configuration

--- flex-app/src/components/SobreMi.jsx
+++ flex-app/src/components/SobreMi.jsx
@@ -1,6 +1,21 @@
+ import FallingText from "./Atomos/FallingText "
+ 
  const SobreMi = () => {
      return (
-        <div>Sobre MI</div>
+        <div className="Sobremi-container">
+            <FallingText
+            text={`No solo diseño webs, con mis 6 años en diseño y desarrollo web creo experiencias digitales que hacen crecer tu negocio, Entrego rápido, sin tecnicismos y siempre te acompaño todo el proceso`}
+            highlightWords={["diseño", "webs", "6", "años", "experiencia", "crecer", "negocio"]}
+            highlightClass="highlighted"
+            trigger="hover"
+            backgroundColor="transparent"
+            wireframes={false}
+            gravity={0.56}
+            fontSize="5rem"
+            mouseConstraintStiffness={0.9}
+            height="100%"
+            />
+        </div>
      )
  }
 
