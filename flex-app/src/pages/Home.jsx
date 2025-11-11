21022198de83d96616865adf758a94b80be80f24
steventapiero24 <steventapiero24@gmail.com>
2025-11-11 15:32:15 +0100

    scroll gsap

--- flex-app/src/pages/Home.jsx
+++ flex-app/src/pages/Home.jsx
@@ -1,28 +1,49 @@
-import React from 'react'
-import Nav from '../components/Nav'
-import Hero from '../components/Hero'
-import SobreNosotros from '../components/SobreNosotros'
-import Benefits from '../components/Projects'
-import CardPrices from '../components/Atomos/CardPrices'
-import SobreMi from '../components/SobreMi'
-import Comparation from '../components/ForWhat'
-import OtherServices from '../components/OtherServices'
-import Footer from '../components/Footer'
+import React, { useLayoutEffect } from "react";
+import Nav from "../components/Nav";
+import Hero from "../components/Hero";
+import SobreNosotros from "../components/SobreNosotros";
+import Benefits from "../components/Projects";
+import CardPrices from "../components/Atomos/CardPrices";
+import SobreMi from "../components/SobreMi";
+import Comparation from "../components/ForWhat";
+import OtherServices from "../components/OtherServices";
+import Footer from "../components/Footer";
+
+import gsap from "gsap";
+import { ScrollTrigger } from "gsap/ScrollTrigger";
+import { ScrollSmoother } from "gsap/ScrollSmoother";
+
+
+gsap.registerPlugin(ScrollTrigger, ScrollSmoother);
 
 const Home = () => {
-  return (
-    <>
-     <Nav />
-     <Hero  />  
-     <Benefits />
-     <SobreNosotros />
-     <CardPrices />
-     <SobreMi />
-     <Comparation />
-     <OtherServices />  
-     <Footer />
-    </>
-  )
-}
+  useLayoutEffect(() => {
+    if (ScrollSmoother.get()) ScrollSmoother.get().kill();
 
-export default Home
+    // Crear el efecto de scroll suave
+    ScrollSmoother.create({
+      wrapper: "#smooth-wrapper",
+      content: "#smooth-content",
+      smooth: 1, 
+      effects: true,
+    });
+  }, []);
+
+  return (
+    <div id="smooth-wrapper">
+      <div id="smooth-content">
+        <Nav />
+        <Hero />
+        <Benefits />
+        <SobreNosotros />
+        <CardPrices />
+        <SobreMi />
+        <Comparation />
+        <OtherServices />
+        <Footer />
+      </div>
+    </div>
+  );
+};
+
+export default Home;
